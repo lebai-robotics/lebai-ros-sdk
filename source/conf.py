0183b5521c0c0df3398064c208751b8cbbb79fb2
liufang <liufang_robot@outlook.com>
2022-04-13 16:43:26 +0800

    update document.

--- source/conf.py
+++ source/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx_copybutton',
     'sphinx.ext.githubpages',
+    'sphinx_tabs.tabs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
