07aede12220c97e4cacd25067c082fb6bd50c849
liufang-robot <61956285+liufang-robot@users.noreply.github.com>
2026-07-09 13:44:43 +0800

    fix: align moveit execution timeout (#56)

--- lebai_lm3_moveit_config/launch/lm3.launch.py
+++ lebai_lm3_moveit_config/launch/lm3.launch.py
@@ -206,8 +206,8 @@ def generate_launch_description():
 
     trajectory_execution = {
         "moveit_manage_controllers": True,
-        "trajectory_execution.allowed_execution_duration_scaling": 1.2,
-        "trajectory_execution.allowed_goal_duration_margin": 0.5,
+        "trajectory_execution.allowed_execution_duration_scaling": 1.25,
+        "trajectory_execution.allowed_goal_duration_margin": 5.0,
         "trajectory_execution.allowed_start_tolerance": 0.01,
     }
 
